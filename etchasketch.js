12f3c98d48e371a9e8ff689c16b8218a7fa30d12
Callaghanjake <jake_2024@hotmail.co.uk>
2023-03-28 16:20:21 +0100

    Created 'active' CSS class to change div color on mouseover. Created mouseover event within forloop to change div color in Javascript file.

--- etchasketch.js
+++ etchasketch.js
@@ -1,18 +1,18 @@
 let frame = document.querySelector('.frame');
 
-let div = document.createElement('div');
-  div.classList.add('box');
-
-
-// frame.appendChild(div);
-
-let num = 4;
+let num = 10;
 let squared = num * num;
 
 for (let i = 0; i < squared; i++) {
     let div = document.createElement('div');
     div.classList.add('box');
-    div.textContent = 'hi';
     frame.appendChild(div);
 
+    div.onmouseover = function() {
+        div.classList.add('active');
+    }
 }
+
+
+// Need to dynamically change CSS grid-template-columns repeat() number
+// grid-template-columns: repeat(?, 1fr) needs to be the same as num
