be3b35e794176410c3c351b5598148f49e94c4da
Callaghanjake <jake_2024@hotmail.co.uk>
2023-03-28 22:24:44 +0100

    Annotated the code with comments.

--- etchasketch.js
+++ etchasketch.js
@@ -1,23 +1,23 @@
-let frame = document.querySelector('.frame');
+// Query selector to grab HTML frame DIV.
+  let frame = document.querySelector('.frame');
 
-let num = 16;
-let squared = num * num;
+// Variables that create the player grid.
+  let num = 16;
+  let squared = num * num;
 
-function changeGridSize() {
+// Change the CSS GRID-TEMPLATE-COLUMNS based on the NUM variable above.
+  function changeGridSize() {
     frame.style.setProperty('grid-template-columns', 'repeat(' + num + ', 1fr)');
-}
+  }
 
-for (let i = 0; i < squared; i++) {
+// FOR LOOP to create DIV squares based on NUM and SQUARED variables.
+  for (let i = 0; i < squared; i++) {
     let div = document.createElement('div');
     div.classList.add('box');
     frame.appendChild(div);
-
-    div.onmouseover = function() {
+    
+    div.onmouseover = function() { // Change the color of player grid squares to black on mouseover.
         div.classList.add('active');
     }
-    
-}
-changeGridSize();
-
-// Need to dynamically change CSS grid-template-columns repeat() number
-// grid-template-columns: repeat(?, 1fr) needs to be the same as num
+  }
+  changeGridSize();
