9958c1a0a9605ced92fb2c02ddab70ca783f932a
Callaghanjake <jake_2024@hotmail.co.uk>
2023-03-28 22:07:07 +0100

    Created a Javascript function to change the size of the play grid by matching the num variable's number with the CSS grid-template-columns repeat().

--- etchasketch.js
+++ etchasketch.js
@@ -1,7 +1,11 @@
 let frame = document.querySelector('.frame');
 
-let num = 10;
+let num = 16;
 let squared = num * num;
+
+function changeGridSize() {
+    frame.style.setProperty('grid-template-columns', 'repeat(' + num + ', 1fr)');
+}
 
 for (let i = 0; i < squared; i++) {
     let div = document.createElement('div');
@@ -11,8 +15,9 @@ for (let i = 0; i < squared; i++) {
     div.onmouseover = function() {
         div.classList.add('active');
     }
+    
 }
-
+changeGridSize();
 
 // Need to dynamically change CSS grid-template-columns repeat() number
 // grid-template-columns: repeat(?, 1fr) needs to be the same as num
